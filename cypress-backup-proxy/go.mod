0425cc4692030e6a6d43e0cb87181fd2017ae8d4
Soner Sayakci <s.sayakci@shopware.com>
2022-04-12 19:18:49 +0000

    fix mysql dumping for old docker-compose version

--- cypress-backup-proxy/go.mod
+++ cypress-backup-proxy/go.mod
@@ -3,19 +3,15 @@ module cypress-backup-proxy
 go 1.16
 
 require (
-	github.com/Microsoft/go-winio v0.5.1 // indirect
-	github.com/containerd/containerd v1.5.8 // indirect
-	github.com/docker/docker v20.10.12+incompatible
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/containerd/containerd v1.6.2 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.14+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
-	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
-	golang.org/x/net v0.0.0-20211215060638-4ddde0e984e9 // indirect
-	golang.org/x/sys v0.0.0-20211214234402-4825e8c3871d // indirect
-	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
-	golang.org/x/tools v0.1.5 // indirect
-	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
-	google.golang.org/grpc v1.43.0 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
+	golang.org/x/sys v0.0.0-20220412071739-889880a91fd5 // indirect
+	google.golang.org/genproto v0.0.0-20220407144326-9054f6ed7bac // indirect
 )
--- cypress-backup-proxy/main.go
+++ cypress-backup-proxy/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
@@ -27,116 +28,102 @@ func init() {
 	}
 }
 
-func restoreHandler(w http.ResponseWriter, r *http.Request)  {
+func restoreHandler(w http.ResponseWriter, r *http.Request) {
 	host := strings.Split(r.Host, ".")[0]
 
-	opts := types.ContainerListOptions{All: true}
-	opts.Filters = filters.NewArgs()
-	opts.Filters.Add("name", "shopware-docker-mysql-1")
-
-	containers, err := dClient.ContainerList(context.Background(), opts)
-
+	c, err := getMysqlContainer()
 	if err != nil {
 		w.WriteHeader(500)
 		fmt.Fprintln(w, err)
 		return
 	}
 
-	for _, c := range containers {
-		log.Printf("Performing restoring for %s\n", host)
+	log.Printf("Performing restoring for %s\n", host)
 
-		execConfig := types.ExecConfig{
-			AttachStderr: true,
-			AttachStdin:  true,
-			AttachStdout: true,
-			Cmd: []string{"bash", "-c", fmt.Sprintf("mysql -uroot -p%s < /tmp/%s.sql", os.Getenv("MYSQL_ROOT_PASSWORD"), host)},
-			Tty:          true,
-			Detach:       false,
-		}
-
-		create, err := dClient.ContainerExecCreate(ctx, c.ID, execConfig)
-		if err != nil {
-			w.WriteHeader(500)
-			log.Println(err)
-			fmt.Fprintln(w, err)
-			return
-		}
-
-		execAttachConfig := types.ExecStartCheck{
-			Detach: false,
-			Tty:    true,
-		}
-
-		containerResponse, err := dClient.ContainerExecAttach(ctx, create.ID, execAttachConfig)
-		if err != nil {
-			w.WriteHeader(500)
-			log.Println(err)
-			fmt.Fprintln(w, err)
-			return
-		}
-
-		defer containerResponse.Close()
-
-		data, _ := ioutil.ReadAll(containerResponse.Reader)
-		log.Printf("Output: %s\n", string(data))
-
-		log.Printf("Completed restoring for %s\n", host)
+	execConfig := types.ExecConfig{
+		AttachStderr: true,
+		AttachStdin:  true,
+		AttachStdout: true,
+		Cmd:          []string{"bash", "-c", fmt.Sprintf("mysql -uroot -p%s < /tmp/%s.sql", os.Getenv("MYSQL_ROOT_PASSWORD"), host)},
+		Tty:          true,
+		Detach:       false,
 	}
+
+	create, err := dClient.ContainerExecCreate(ctx, c.ID, execConfig)
+	if err != nil {
+		w.WriteHeader(500)
+		log.Println(err)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	execAttachConfig := types.ExecStartCheck{
+		Detach: false,
+		Tty:    true,
+	}
+
+	containerResponse, err := dClient.ContainerExecAttach(ctx, create.ID, execAttachConfig)
+	if err != nil {
+		w.WriteHeader(500)
+		log.Println(err)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	defer containerResponse.Close()
+
+	data, _ := ioutil.ReadAll(containerResponse.Reader)
+	log.Printf("Output: %s\n", string(data))
+
+	log.Printf("Completed restoring for %s\n", host)
 
 	fmt.Fprint(w, "success")
 }
 
-func backupHandler(w http.ResponseWriter, r *http.Request)  {
+func backupHandler(w http.ResponseWriter, r *http.Request) {
 	host := strings.Split(r.Host, ".")[0]
 
-	opts := types.ContainerListOptions{All: true}
-	opts.Filters = filters.NewArgs()
-	opts.Filters.Add("name", "shopware-docker-mysql-1")
-
-	containers, err := dClient.ContainerList(context.Background(), opts)
-
+	c, err := getMysqlContainer()
 	if err != nil {
 		w.WriteHeader(500)
 		fmt.Fprintln(w, err)
 		return
 	}
 
-	for _, c := range containers {
-		log.Printf("Performing backup for %s\n", host)
+	log.Printf("Performing backup for %s\n", host)
 
-		execConfig := types.ExecConfig{
-			AttachStderr: true,
-			AttachStdin:  true,
-			AttachStdout: true,
-			Cmd: []string{"bash", "-c", fmt.Sprintf("mysqldump --add-drop-database -uroot -p%s --databases %s > /tmp/%s.sql", os.Getenv("MYSQL_ROOT_PASSWORD"), host, host)},
-			Tty:          true,
-			Detach:       false,
-		}
-
-		create, err := dClient.ContainerExecCreate(ctx, c.ID, execConfig)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintln(w, err)
-			return
-		}
-
-		execAttachConfig := types.ExecStartCheck{
-			Detach: false,
-			Tty:    true,
-		}
-
-		containerResponse, err := dClient.ContainerExecAttach(ctx, create.ID, execAttachConfig)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintln(w, err)
-			return
-		}
-
-		defer containerResponse.Close()
-
-		data, _ := ioutil.ReadAll(containerResponse.Reader)
-		log.Println(string(data))
+	execConfig := types.ExecConfig{
+		AttachStderr: true,
+		AttachStdin:  true,
+		AttachStdout: true,
+		Cmd:          []string{"bash", "-c", fmt.Sprintf("mysqldump --add-drop-database -uroot -p%s --databases %s > /tmp/%s.sql", os.Getenv("MYSQL_ROOT_PASSWORD"), host, host)},
+		Tty:          true,
+		Detach:       false,
 	}
+
+	create, err := dClient.ContainerExecCreate(ctx, c.ID, execConfig)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	execAttachConfig := types.ExecStartCheck{
+		Detach: false,
+		Tty:    true,
+	}
+
+	containerResponse, err := dClient.ContainerExecAttach(ctx, create.ID, execAttachConfig)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	defer containerResponse.Close()
+
+	data, _ := ioutil.ReadAll(containerResponse.Reader)
+	log.Println(string(data))
 
 	fmt.Fprint(w, "success")
 }
@@ -147,4 +134,36 @@ func main() {
 	http.HandleFunc("/cleanup", restoreHandler)
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
+
+func getMysqlContainer() (*types.Container, error) {
+	opts := types.ContainerListOptions{All: true}
+	opts.Filters = filters.NewArgs()
+	opts.Filters.Add("name", "shopware-docker-mysql-1")
+
+	containers, err := dClient.ContainerList(context.Background(), opts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, container := range containers {
+		return &container, nil
+	}
+
+	opts = types.ContainerListOptions{All: true}
+	opts.Filters = filters.NewArgs()
+	opts.Filters.Add("name", "shopware-docker_mysql_1")
+
+	containers, err = dClient.ContainerList(context.Background(), opts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, container := range containers {
+		return &container, nil
+	}
+
+	return nil, errors.New("cannot find mysql container")
 }
